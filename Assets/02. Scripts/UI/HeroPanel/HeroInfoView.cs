3c75a71efdf5e8f87a26c8482295fa08e45ee4fe
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2026-07-08 10:05:49 +0900

    ✨ feat:"(임시) HeroInfo UI 구현"

---
+++ Assets/02. Scripts/UI/HeroPanel/HeroInfoView.cs
@@ -0,0 +1,43 @@
+using LuckyDefense.Heroes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace LuckyDefense.UI.HeroPanel
+{
+    public class HeroInfoView : MonoBehaviour
+    {
+        [SerializeField] private GameObject root;
+
+        [SerializeField] private Image icon;
+
+        [SerializeField] private TextMeshProUGUI heroName;
+
+        [SerializeField] private TextMeshProUGUI attack;
+
+        [SerializeField] private TextMeshProUGUI attackSpeed;
+
+        [SerializeField] private TextMeshProUGUI grade;
+
+        public void Show()
+        {
+            root.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            root.SetActive(false);
+        }
+
+        public void Refresh(Hero hero)
+        {
+            heroName.text = hero.Data.HeroName;
+            attack.text = hero.Stats.Attack.ToString();
+            attackSpeed.text = hero.Stats.AttackSpeed.ToString("0.00");
+            grade.text = hero.Data.Grade.ToString();
+
+            // icon.sprite = hero.Data.Icon;
+        }
+    }
+}
